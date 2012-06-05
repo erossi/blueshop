a619227a768af6940d0301ee0015b95cdbde2daf
Enrico Rossi <e.rossi@tecnobrain.com>
2012-06-05 15:15:00 +0200

    Fix path, email address and README.

--- runme.sh
+++ runme.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-export BLUESHOP_CFG="/home/enrico/blueshop/cfg"
+INSTALL=/my_installation_path/blueshop
+export BLUESHOP_CFG=$INSTALL/cfg
 export BLUESHOP_DEBUG="true"
 
-cd bin/
-python blueshop.py
+if [ -e $BLUESHOP_CFG/blueshop.cfg ]; then
+	cd $INSTALL/bin
+	python blueshop.py
+else
+	echo "Configuration file not found! Please fix."
+fi;
 
