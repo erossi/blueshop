0087c1ef77979edd5ed982c5c4279629a8ca3c5a
Enrico Rossi <enrico.rossi@ext.ec.europa.eu>
2014-07-01 11:51:31 +0200

    starting redesign the package layout.
     * moved the python modules into a modules dir.
     * starting a new setup.py

---
+++ tools/update_version.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+# Update the version.py file with the current GIT version.
+
+FILENAME="modules/__init__.py"
+
+if (type git > /dev/null 2>&1); then
+    MAJOR=$(git describe --tags|sed -e "s/^\([^-]*\).*/\1/")
+    TAGGED=$(git describe --tags|sed -e "s/^\(.*\)-.*/\1/")
+    FULL=$(git describe --tags)
+
+    echo "_version_ = \"$MAJOR\"" > $FILENAME
+    echo "_tagged_version_ = \"$TAGGED\"" >> $FILENAME
+    echo "_git_version_ = \"$FULL\"" >> $FILENAME
+    echo "_lastupdate_ = \"$(date +%d/%m/%Y)\"" >> $FILENAME
+else
+    exit 1
+fi
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
