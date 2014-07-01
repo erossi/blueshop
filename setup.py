d50af46f0df7f0d06dbd9faa5c782ec2e632cdda
Enrico Rossi <enrico.rossi@ext.ec.europa.eu>
2014-07-01 15:44:31 +0200

    fix setup install path to /opt

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-# Copyright (C) 2012, 2013 Enrico Rossi
+# coding: utf-8
+
+# Copyright (C) 2012-2014 Enrico Rossi
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
@@ -15,50 +17,86 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
+from distutils.command.sdist import sdist
 from glob import glob
 import os
+import fnmatch
+from subprocess import call
+
+# Update the version needed also for the package name.
+# FIXME isn't any other better way to do this?
+if call('tools/update_version.sh', shell=True):
+    print('Error updating version.')
+
+from modules import _version_
+
+class SdistOverride(sdist):
+    """ Useless now that the updated version is called before
+    """
+    def run(self):
+        print('Updating version from git.')
+
+        if call('tools/update_version.sh', shell=True):
+            print('Error updating version.')
+        else:
+            sdist.run(self)
+
+def select_all_files(folder, matchfilter):
+    """ Scan subdirectory and select all the matching files.
+
+    Return: a list of tuple compatibile with the setup list.
+    """
+    result = []
+
+    for root, dirnames, filenames in os.walk(folder):
+        selected_files = []
+
+        for filename in fnmatch.filter(filenames, matchfilter):
+            selected_files.append(os.path.join(root, filename))
+
+        if len(selected_files):
+            result.append((root, selected_files))
+
+    return (result)
+
+def list_data_files():
+    """ Create the data_files list for setup.
+    """
+    data_files = [
+        ('db', glob(os.path.join('./db', '*.dump'))),
+        ('private', ['private/stock.xls', 'private/vip.xls',
+            'private/eu.xls']),
+        ('etc', ['cfg/blueshop.cfg', 'cfg/mail_pricelist.txt',
+            'cfg/mail_promo.txt']),
+        ('images', ['images/favicon.ico']),
+        ('static', glob(os.path.join('./static', 'x*'))),
+        ('static/javascripts',
+            glob(os.path.join('./static/javascripts', '*.js'))),
+        ('static/stylesheets',
+            glob(os.path.join('./static/stylesheets', '*.css'))),
+#        ('tools', ['tools/update_search_engine.py']),
+        ('doc',
+            glob(os.path.join('doc', '*.txt'))),
+        ('doc/example',
+            glob(os.path.join('doc/example', '*')))
+        ]
+
+    data_files += select_all_files('views', '*.tpl')
+    data_files += select_all_files('images', '*.png')
+    data_files += select_all_files('images', '*.gif')
+    data_files += select_all_files('locale', '*.mo')
+    # print 'data_files = ', data_files
+    return(data_files)
 
 setup(name='blueshop',
-        version='2.0',
+        version=_version_,
         description='Yet Another small web shop',
         author='Enrico Rossi',
         author_email='e.rossi@tecnobrain.com',
         url='http://enricorossi.org/blueshop/',
         packages=['blueshop'],
-        package_dir={'blueshop': 'bin'},
-        scripts=['bin/blueshop'],
-        data_files=[
-            ('/var/local/blueshop/db', ['db/users.sqlite3.dump',
-                'db/shop.sqlite3.dump']),
-            ('/var/local/blueshop/private', ['private/stock.xls',
-                'private/vip.xls', 'private/eu.xls']),
-            ('etc/blueshop', ['cfg/blueshop.cfg',
-                'cfg/mail_pricelist.txt',
-                'cfg/mail_promo.txt']),
-            ('share/blueshop/views',
-                glob(os.path.join('bin/views', '*.tpl'))),
-            ('share/blueshop/views/admin',
-                glob(os.path.join('bin/views/admin', '*.tpl'))),
-            ('share/blueshop/views/mail',
-                glob(os.path.join('bin/views/mail', '*.tpl'))),
-            ('share/blueshop/views/main',
-                glob(os.path.join('bin/views/main', '*.tpl'))),
-            ('share/blueshop/views/store',
-                glob(os.path.join('bin/views/store', '*.tpl'))),
-            ('share/blueshop/views/user',
-                glob(os.path.join('bin/views/user', '*.tpl'))),
-            ('share/blueshop/images',
-                glob(os.path.join('bin/images', '*.png'))),
-            ('share/blueshop/static',
-                glob(os.path.join('bin/static', 'x*'))),
-            ('share/blueshop/static/javascripts',
-                glob(os.path.join('bin/static/javascripts', '*.js'))),
-            ('share/blueshop/static/stylesheets',
-                glob(os.path.join('bin/static/stylesheets', '*.css'))),
-            ('share/doc/blueshop',
-                glob(os.path.join('doc', '*.txt'))),
-            ('share/doc/blueshop/example',
-                glob(os.path.join('doc/example', '*')))
-            ])
+        package_dir={'blueshop': 'modules'},
+        data_files=list_data_files(),
+        cmdclass={'sdist':SdistOverride})
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
