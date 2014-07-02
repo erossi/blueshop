12af4d98b9a6d2e1284e92934e1a05aa929d58cb
Enrico Rossi <enrico.rossi@ext.ec.europa.eu>
2014-07-02 17:51:58 +0200

    fix update tool.

--- tools/update_version.sh
+++ tools/update_version.sh
@@ -15,8 +15,6 @@ if (type git > /dev/null 2>&1); then
     echo "_tagged_version_ = \"$TAGGED\"" >> $FILENAME
     echo "_git_version_ = \"$FULL\"" >> $FILENAME
     echo "_lastupdate_ = \"$(date +%d/%m/%Y)\"" >> $FILENAME
-else
-    exit 1
 fi
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
